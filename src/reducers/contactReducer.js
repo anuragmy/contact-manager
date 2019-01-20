fe79ff1305a49274bbd670757e054e9946b58b40
= <=>
2019-01-21 02:17:18 +0530

    add contact using axios

--- src/reducers/contactReducer.js
+++ src/reducers/contactReducer.js
@@ -1,32 +1,21 @@
-import { GET_CONTACTS, DELETE_CONTACT, ADD_CONTACT } from '../actions/types';
+import {
+	GET_CONTACTS,
+	DELETE_CONTACT,
+	ADD_CONTACT
+} from '../actions/types';
+import axios from 'axios'
 const initialState = {
-	contacts: [
-		{
-			id: 1,
-			name: 'John Doe',
-			email: 'john@gmail.com',
-			phone: '555-555-5555'
-		},
-		{
-			id: 2,
-			name: 'Karen Williams',
-			email: 'karen@gmail.com',
-			phone: '444-444-4444'
-		},
-		{
-			id: 3,
-			name: 'Henry Johnson',
-			email: 'henry@gmail.com',
-			phone: '333-333-333'
-		}
-	]
+	contacts: []
 };
 
-export default function(state = initialState, action = {}) {
+
+
+export default function (state = initialState, action = {}) {
 	switch (action.type) {
 		case GET_CONTACTS:
 			return {
-				...state
+				...state,
+				contacts: action.payload
 			};
 		case DELETE_CONTACT:
 			return {
@@ -37,7 +26,7 @@ export default function(state = initialState, action = {}) {
 		case ADD_CONTACT:
 			return {
 				...state,
-				contacts: [ action.payload, ...state.contacts ]
+				contacts: [action.payload, ...state.contacts]
 			};
 
 		default:
